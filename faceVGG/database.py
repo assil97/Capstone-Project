558d317af758041a9c5d63d9489572ede0597e1f
Syed Shakeeb Assil <30729905+assil97@users.noreply.github.com>
2019-05-18 03:20:16 +0530

    Add files via upload

--- faceVGG/database.py
+++ faceVGG/database.py
@@ -1,7 +1,9 @@
 import csv
 
+#opening the file and reading the contents in the file row by row
 with open('/Users/syedshakeeb/Desktop/faceVGG/attendance.csv', 'r') as csvFile:
     reader = csv.reader(csvFile)
+    #iterating to print the content row by row
     for row in reader:
         print(row)
 
